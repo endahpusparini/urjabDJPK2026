254254be1357c5041150e909c50926cf81f724d7
endahpusparini <endah9696@gmail.com>
2026-09-21 15:33:34 +0700

    Update Google Sheet ID in script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@
 // 1. MASUKKAN GOOGLE SHEET ID DI SINI
 // =====================================================
 
-const SHEET_ID = "e/2PACX-1vRnqIAvEqNOyIMtJAe3gbqKWF8eJe_LSDZOJtSfloFLLv4rkGPyx6Lc1AQEazEV-ZmpR6MJ7dNdfs4J";
+const SHEET_ID = "1Ny9Mkmo8smsc_ZrXnubIwuGZoiDIHHrL8OHm8dYjFvU";
 
 const SHEET_NAME = "URJAB";
 
